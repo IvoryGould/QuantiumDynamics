0bdf5c92827eceaa6fcb2389debc74dfc3143f97
ErvieKyun <e.nunez91@gmail.com>
2019-08-02 05:08:56 +1000

    CHECKPOINT - LevelDesign | Mechanics
    - Added Interactables
    - Added Interactables Scripts
    - Added temporary #Interact input button [e]
    
    !!! IVORY, PLS FIX AFTER PITCH DAY !!!

---
+++ QuantiumDynamics/Assets/Scripts/DesignerCode/DESIGNER_FeatureTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DESIGNER_FeatureTest : MonoBehaviour
+{
+    private GameObject _cameraFollow;
+    public Vector3 CameraOffset;
+    public float CameraSmoothing;
+
+    private void Start()
+    {
+        _cameraFollow = GameObject.Find("Main Camera");
+        //CameraOffset = 0.3f;
+        if (CameraSmoothing == 0f)
+        {
+            CameraSmoothing = 5f;
+        }
+    }
+    void Update()
+    {
+        CameraController();
+    }
+
+    public void CameraController()
+    {
+        _cameraFollow.transform.position =
+            Vector3.Lerp(_cameraFollow.transform.position, transform.position + CameraOffset, CameraSmoothing * Time.deltaTime);
+    }
+}
