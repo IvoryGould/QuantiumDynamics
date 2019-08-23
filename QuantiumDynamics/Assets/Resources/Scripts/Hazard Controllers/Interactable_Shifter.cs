145d6bc460786cf3ab09d495ed6995f4cecd1d02
ErvieKyun <e.nunez91@gmail.com>
2019-08-23 17:00:39 +1000

    Level Design Push
    - WIP Interactables
    - Added Shifter Puzzle Manager
    - Added Shifter Dummy

---
+++ QuantiumDynamics/Assets/Resources/Scripts/Hazard Controllers/Interactable_Shifter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_Shifter : MonoBehaviour
+{
+    public GameObject[] Gates;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Interact"))
+        {
+            foreach (GameObject Gate in Gates)
+            {
+                Gate.GetComponent<Dummy_Shifter>().Switch();
+            }
+        }
+    }
+}
