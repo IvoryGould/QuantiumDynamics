b131483f5394bd6a15ddaed6f4bd6886b4fb7df8
IvoryGould <ivory.gould@gmail.com>
2019-08-23 17:02:28 +1000

    PlayerController

---
+++ QuantiumDynamics/Assets/Resources/Scripts/GhostCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostCollision : MonoBehaviour
+{
+
+    public bool isColliding;
+
+    private void OnTriggerStay(Collider other) {
+
+        isColliding = true;
+
+    }
+
+}
