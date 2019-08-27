f8031e7aade0838eac668e4c20537a14ef25a0de
ErvieKyun <e.nunez91@gmail.com>
2019-08-28 09:31:50 +1000

    Level Design Push
    - Added Placeholder lasers

---
+++ QuantiumDynamics/Assets/Dummy_LaserBeam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy_LaserBeam : MonoBehaviour
+{
+    [Header("Key Points")]
+    public Transform StartPoint;
+    public Transform EndPoint;
+
+    private LineRenderer _laserLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _laserLine = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _laserLine.SetPosition(0, StartPoint.position);
+        _laserLine.SetPosition(1, EndPoint.position);
+    }
+}
