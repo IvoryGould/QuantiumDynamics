cfef6ae8d2795c872592f87d71a11d71a1f78429
Josh Waterhouse <joshua_waterhouse@hotmail.com>
2019-09-11 14:52:39 +1000

    Journal System
    System to store and collect the journal entries within the game, storing in a list for eventual saving

---
+++ QuantiumDynamics/Assets/Resources/Scripts/NarrativeScripts/NarrativeManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NarrativeManager : MonoBehaviour
+{
+    //List of all terminals
+    public List<bool> terminals;
+    //List of all items that can be picked up
+    public List<bool> items;
+    //List of all logs that can be found and stored
+    public List<bool> logs;
+    public bool entry;
+    public string entryJourn;
+    private GameObject journalBox;
+    private TextMeshProUGUI journalBoxTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entry = terminals[0] = false;
+        entryJourn = "Test 487. \n\nGravity test 128 \n\nAbilities disabled to start, activate in backup chamber";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void EntryEntry()
+    {
+        if (journalBox == null)
+        {
+            journalBox = GameObject.FindGameObjectWithTag("JournalBox");
+            journalBoxTxt = journalBox.GetComponent<TextMeshProUGUI>();
+        }
+        if (entry == true)
+        {
+            journalBoxTxt.text = entryJourn;
+        }
+    }
+}
